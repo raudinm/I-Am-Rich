819f597bb337a9f45c884755db3ccf51c63e3bc4
Raudin Moreno <raudin247@gmail.com>
2020-04-20 14:16:46 -0400

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Raudin Moreno on 4/20/20.
+//  Copyright © 2020 Raudin Moreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
